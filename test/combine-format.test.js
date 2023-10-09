84e19e65b1f16e0d39b6fae39c26f3a9f95103b9
Yosua Marchel <marchel.yosua@gmail.com>
2023-10-09 17:52:40 +0700

    push hasil belajar nodejs logging

---
+++ test/combine-format.test.js
@@ -0,0 +1,36 @@
+import winston from "winston";
+
+test ("logging with combine format", () => {
+
+    const logger = winston.createLogger({
+        level: "info",
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            winston.format.json()
+        ),
+
+        transports: [
+            new winston.transports.Console({})
+        ]
+    });
+
+    logger.info("Hello Format");
+    logger.warn("Hello Format");
+    logger.error("Hello Format");
+});
+
+test ("logging with format", () => {
+
+    const logger = winston.createLogger({
+        level: "info",
+        // format: winston.format.json(),
+        // format: winston.format.simple(),
+        format: winston.format.logstash(),
+        transports: [
+            new winston.transports.Console({})
+        ]
+    });
+
+    logger.info("Hello Format");
+});
